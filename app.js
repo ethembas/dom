7c9825529f1442d936974aa1c1d69b66c0c05caa
ethembas <77681780+ethembas@users.noreply.github.com>
2021-03-11 06:00:49 -0800

    update

---
+++ app.js
@@ -0,0 +1,32 @@
+let formEl1=document.createElement('form');
+let button1=document.createElement('button');
+let input1=document.createElement('input');
+
+formEl1.appendChild(input1);
+formEl1.appendChild(button1);
+let divform=document.createElement('div');
+divform.appendChild(formel1);
+
+let divMain=document.querySelector('#main');
+divMain.appendChild(divform);
+
+
+//
+
+let div2=document.createElement('div');
+
+let ul=document.createElement('ul');
+
+let li=document.createElement('li');
+
+ul.appendChild(li);
+div2.appendChild(ul);
+
+divMain.appendChild(div2);
+
+
+
+
+
+
+
